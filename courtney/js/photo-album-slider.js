dc3490743978e1256997e5db510db60818584ae0
dylanmoh <dylanmoh94@gmail.com>
2018-01-17 17:46:30 -0600

    slider overlay

--- courtney/js/photo-album-slider.js
+++ courtney/js/photo-album-slider.js
@@ -1,101 +1,130 @@
 jQuery(document).ready(function () {
-	var slider = $('.photos_list');
-	var sliderWidth = $('.photo-slider').width();
-	var leftArrow = $('.arrow_left');
-	var rightArrow = $('.arrow_right');
 
-    var widthsArr = new Array();
+    var mySlider = new Slider(this);
+    mySlider.initSlider();
+});
+
+function Slider(slider) {
+    $('.content_centered').height(($(document).height()) - ($('.site-header').height()));
+    var slider = $('.photos_list');
+    var sliderWidth = $('.photo-slider').width();
+    var leftArrow = $('.arrow_left');
+    var rightArrow = $('.arrow_right');
+
+    var first = $(slider[0].children[0]);
+    var second = $(slider[0].children[1]);
+    var last = $(slider[0].children[slider[0].childElementCount - 1]);
+    var secondLast = $(slider[0].children[slider[0].childElementCount - 2]);
+
+    first.clone().appendTo(slider);
+    second.clone().appendTo(slider);
+    last.clone().prependTo(slider);
+    secondLast.clone().prependTo(slider);
+
+    var positionArr = new Array();
+    var tempPrevPosition = 0;
+    var tempCurrentPosition = $('.photo_album_wrap').width() / 2;
     for (var i = 0; i < slider[0].childElementCount; i++) {
-        if ($($('.photos_list div')[i]).hasClass('portrait')) {
-            widthsArr.push('portrait');
-        }
-        else {
-            widthsArr.push('landscape');
-        }
+            tempCurrentPosition -= tempPrevPosition;
+            tempPrevPosition = slider[0].children[i].clientWidth / 2;
+            tempCurrentPosition -= tempPrevPosition;
+            positionArr.push(tempCurrentPosition);
+    }
+    var gallery = $('.photo_gallery div');
+    var currentIndex = 2;
+    var firstIndex = 2;
+    var lastIndex = slider[0].childElementCount - 3;
+    var currentLeft = $('.photo_album_wrap').width() / 2;
+
+    this.initSlider = function() {
+        $(gallery[0]).addClass('photo_gallery_item_selected');
+        $('.arrow_div').width(($('.photo_album_wrap').width() - (slider[0].children[currentIndex].scrollWidth)) / 2);
+        slider.css({left: positionArr[currentIndex]});
+        this.initSliderClickEvents();
+        this.initSliderResizeEvents();
+        this.initHorizontalScroll();
+        this.initGalleryClickEvents();
     }
 
-	var gallery = $('.photo_gallery div');
+    this.initSliderClickEvents = function() {
+        $(slider).click(function() {
+            document.getElementById("picture-overlay").style.display = "block";
+            $('.picture-overlay__image').css("background-image", "url(" + $(slider[0].children[currentIndex].children[0])[0].currentSrc + ")");
+            $('.picture-overlay__image').css("background-size", "contain");
+            $('.picture-overlay__image').css("background-position", "center");
+            $('.picture-overlay__image').css("background-repeat", "no-repeat");
 
-	var currentIndex = 0;
-	var firstIndex = 0;
-	var lastIndex = slider[0].childElementCount - 1;
-    var currentLeft = $('.photo_album_wrap').width() / 2;
-	leftArrow.hide();
-	$(gallery[0]).addClass('photo_gallery_item_selected');
-    $('.arrow_div').width(($('.photo_album_wrap').width() - (slider[0].children[0].scrollWidth)) / 2);
-    currentLeft -= (slider[0].children[0].scrollWidth / 2);
-    slider.css({left: currentLeft});
-
-
-    $(rightArrow).click(function() {
-        if (currentIndex != lastIndex) {
-        	$(gallery[currentIndex]).removeClass('photo_gallery_item_selected');
-        	currentLeft = slider.position().left;
-            if (currentIndex == firstIndex) {
-        		leftArrow.show();
-        	}
-            currentLeft -= (slider[0].children[currentIndex].scrollWidth / 2);
-        	currentIndex++;
-            currentLeft -= (slider[0].children[currentIndex].scrollWidth / 2);
-
-        	$(gallery[currentIndex]).addClass('photo_gallery_item_selected');
+        })
+        $('#picture-overlay').click(function() {
+            document.getElementById("picture-overlay").style.display = "none";
+        })
+        $(rightArrow).click(function() {
+            $(gallery[currentIndex]).removeClass('photo_gallery_item_selected');
+            currentIndex++;
+            $(gallery[currentIndex]).addClass('photo_gallery_item_selected');
             $('.arrow_div').animate({width: (($('.photo_album_wrap').width() - (slider[0].children[currentIndex].scrollWidth)) / 2)}, 600);
-        	slider.animate({left: currentLeft}, 600);
-        	if (currentIndex == lastIndex) {
-        		rightArrow.hide();
-        	}
-        }
-    })
-    $(leftArrow).click(function() {
-    	$(gallery[currentIndex]).removeClass('photo_gallery_item_selected');
-    	if (currentIndex == lastIndex) {
-    		rightArrow.show();
-    	}
-        currentLeft += (slider[0].children[currentIndex].scrollWidth / 2);
-    	currentIndex--;
-        currentLeft += (slider[0].children[currentIndex].scrollWidth / 2);
-    	$(gallery[currentIndex]).addClass('photo_gallery_item_selected');
-        $('.arrow_div').animate({width: (($('.photo_album_wrap').width() - (slider[0].children[currentIndex].scrollWidth)) / 2)}, 600);
-    	slider.animate({left: currentLeft}, 600);
-    	if (currentIndex == firstIndex) {
-    		leftArrow.hide();
-    	}
-    })
+            slider.animate({left: positionArr[currentIndex]}, 600);
+            if (currentIndex == lastIndex + 1) {
+                currentIndex = firstIndex;
+                slider.animate({left: (positionArr[currentIndex])}, 0);
+            }
+        })
+        $(leftArrow).click(function() {
+            $(gallery[currentIndex]).removeClass('photo_gallery_item_selected');
+            currentIndex--;
+            $(gallery[currentIndex]).addClass('photo_gallery_item_selected');
+            $('.arrow_div').animate({width: (($('.photo_album_wrap').width() - (slider[0].children[currentIndex].scrollWidth)) / 2)}, 600);
+            slider.animate({left: positionArr[currentIndex]}, 600);
+            if (currentIndex == firstIndex - 1) {
+                currentIndex = lastIndex;
+                slider.animate({left: (positionArr[currentIndex])}, 0);
+            }
+        })
+    }
 
+    this.initSliderResizeEvents = function() {
+        $(window).resize(function () {
+            positionArr = new Array();
+            tempPrevPosition = 0;
+            tempCurrentPosition = $('.photo_album_wrap').width() / 2;
+            for (var i = 0; i < slider[0].childElementCount; i++) {
+                    tempCurrentPosition -= tempPrevPosition;
+                    tempPrevPosition = slider[0].children[i].clientWidth / 2;
+                    tempCurrentPosition -= tempPrevPosition;
+                    positionArr.push(tempCurrentPosition);
+            }
+            $('.arrow_div').width(($('.photo_album_wrap').width() - (slider[0].children[currentIndex].scrollWidth)) / 2);
+            slider.animate({left: (positionArr[currentIndex])}, 0);
+        })
+    }
 
-	$(window).resize(function () {
-        var newLeft = $('.photo_album_wrap').width() / 2;
-        newLeft -= (slider[0].children[0].scrollWidth / 2);
-        for (var i = 0; i < currentIndex;) {
-            newLeft -= (slider[0].children[i].scrollWidth / 2);
-            console.log(i);
-            i++;
-            newLeft -= (slider[0].children[i].scrollWidth / 2);
-        }
-        currentLeft = newLeft;
-        $('.arrow_div').width(($('.photo_album_wrap').width() - (slider[0].children[currentIndex].scrollWidth)) / 2);
-    	slider.css({left: (currentLeft)});
-	})
+    this.initHorizontalScroll = function() {
+        var lastScrollLeft
+        $('.photo_album_wrap').scroll(function() {
 
+        })
+    }
 
-    $('.photo_gallery_item').click(function() {
-    	document.body.scrollTop = 0; // For Chrome, Safari and Opera 
-        document.documentElement.scrollTop = 0; // For IE and Firefox
-    	$(gallery[currentIndex]).removeClass('photo_gallery_item_selected');
-    	if (currentIndex == firstIndex) {
-    		leftArrow.show();
-    	}
-    	if (currentIndex == lastIndex) {
-    		rightArrow.show();
-    	}
-    	currentIndex = gallery.index(this);
-    	$(gallery[currentIndex]).addClass('photo_gallery_item_selected');
-    	if (currentIndex == lastIndex) {
-    		rightArrow.hide();
-    	}
-    	if (currentIndex == firstIndex) {
-    		leftArrow.hide();
-    	}
-    	slider.animate({left: (-(sliderWidth * currentIndex))}, 300);
-	})
-});
+    this.initGalleryClickEvents = function() {
+        $('.photo_gallery_item').click(function() {
+            document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+            document.documentElement.scrollTop = 0; // For IE and Firefox
+            $(gallery[currentIndex]).removeClass('photo_gallery_item_selected');
+            if (currentIndex == firstIndex) {
+                leftArrow.show();
+            }
+            if (currentIndex == lastIndex) {
+                rightArrow.show();
+            }
+            currentIndex = gallery.index(this);
+            $(gallery[currentIndex]).addClass('photo_gallery_item_selected');
+            if (currentIndex == lastIndex) {
+                rightArrow.hide();
+            }
+            if (currentIndex == firstIndex) {
+                leftArrow.hide();
+            }
+            slider.animate({left: (-(sliderWidth * currentIndex))}, 300);
+        })
+    }
+}
--- courtney/single-album.php
+++ courtney/single-album.php
@@ -13,34 +13,39 @@
 get_header(); 
 $images = get_field('photo_album');
 ?>
-
-<div class="photo_album_wrap">
-	<div class="photo_album_wrap_div arrow_div arrow_left">
-		<div class="left-arrow">
-			<i class="fa fa-chevron-left" aria-hidden="true"></i>
-		</div>
+<div id="picture-overlay">
+	<div class="picture-overlay__image">
 	</div>
-	<div class="photo_album_wrap_div photo-slider">
-		<?php if ($images) { ?>
-			<div class="photos_list">
-				<?php foreach( $images as $image ) { 
-					if ($image[width] > $image[height]) {
-						$extraClass = "landscape";
-					}
-					else {
-						$extraClass = "portrait";
-					}
-					?>
-		            <div class="photo_item <?php echo $extraClass; ?>">
-		                <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-		            </div>
-        		<?php } ?>
+</div>
+<div class="content_centered">
+	<div class="photo_album_wrap">
+		<div class="photo_album_wrap_div arrow_div arrow_left">
+			<div class="left-arrow">
+				<i class="fa fa-chevron-left" aria-hidden="true"></i>
 			</div>
-		<?php } ?>
-	</div>
-	<div class="photo_album_wrap_div arrow_div arrow_right">
-		<div class="right-arrow">
-			<i class="fa fa-chevron-right" aria-hidden="true"></i>
+		</div>
+		<div class="photo_album_wrap_div photo-slider">
+			<?php if ($images) { ?>
+				<div class="photos_list">
+					<?php foreach( $images as $image ) { 
+						if ($image[width] > $image[height]) {
+							$extraClass = "landscape";
+						}
+						else {
+							$extraClass = "portrait";
+						}
+						?>
+			            <div class="photo_item <?php echo $extraClass; ?>">
+			                <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+			            </div>
+	        		<?php } ?>
+				</div>
+			<?php } ?>
+		</div>
+		<div class="photo_album_wrap_div arrow_div arrow_right">
+			<div class="right-arrow">
+				<i class="fa fa-chevron-right" aria-hidden="true"></i>
+			</div>
 		</div>
 	</div>
 </div>
