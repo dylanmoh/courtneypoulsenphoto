b0791f9b35ce7fd760b494a901f64f83e8cfe17c
dylanmoh <dylanmoh94@gmail.com>
2018-01-18 10:29:44 -0600

    contact page

--- courtney/functions.php
+++ courtney/functions.php
@@ -417,6 +417,7 @@ function twentyseventeen_scripts() {
 	wp_enqueue_style( 'header-css', get_theme_file_uri("/css/header.css"));
 	wp_enqueue_style( 'footer-css', get_theme_file_uri("/css/footer.css"));
 	wp_enqueue_style( 'home-css', get_theme_file_uri("/css/home.css"));
+	wp_enqueue_style( 'contact-css', get_theme_file_uri("/css/contact.css"));
 	wp_enqueue_style( 'album-css', get_theme_file_uri("/css/album.css"));
 	wp_enqueue_script('jQuery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');
 	wp_enqueue_script('font-awesome-js', "https://use.fontawesome.com/94db5156dc.js");
--- courtney/template/contact.php
+++ courtney/template/contact.php
@@ -2,6 +2,33 @@
 /* Template Name: Contact */ 
 
 get_header(); 
-the_content();
 ?>
+<div class="wrap">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<div class="contact-page-wrap">
+				<div class="contact-wrap">
+					<label class="name">
+						<?php echo get_field('name'); ?>
+					</label>
+					<label class="phone">
+						<?php echo get_field('phone_number'); ?>
+					</label>
+					<label class="location">
+						<?php echo get_field('location'); ?>
+					</label>
+				</div>
+				<?php
+				the_post();
+				?>
+				<div class="form-wrap">
+					<?php
+					the_content();
+					?>
+				</div>
+			</div>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!-- .wrap -->
 <?php get_footer();
