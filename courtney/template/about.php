e9215586334281d0324ac4948ccc30fa76912406
dylanmoh <dylanmoh94@gmail.com>
2018-01-18 17:19:06 -0600

    about page css change

--- courtney/template/about.php
+++ courtney/template/about.php
@@ -3,6 +3,7 @@
 
 get_header(); 
 $image = get_field("about_image");
+$beautiful = get_field("me_image");
 ?>
 <div class="about-page">
 	<div id="primary" class="content-area">
@@ -10,10 +11,13 @@ $image = get_field("about_image");
 			<div class="about-page-wrap" style="<?php echo 'background-image: url(' . $image . ')' ?>">
 				<div class="about-content">
 					<h1 class="about-page-header">About Me</h1>
+					<div class="about-left-wrap">
+						<img class="me_image" src="<?php echo $beautiful; ?>" />
+					</div>
 					<?php
 					the_post();
 					?>
-					<div class="about-wrap">
+					<div class="about-right-wrap">
 						<?php
 						the_content();
 						?>
